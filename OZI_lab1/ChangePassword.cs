8b69dce7315ad0cad8a6082ca0e3c3732eaf01f6
deyad <deyadecember@gmail.com>
2020-10-18 22:15:39 +0300

    initial commit

---
+++ OZI_lab1/ChangePassword.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OZI_lab1
+{
+    public partial class ChangePassword : Form
+    {
+        User user = new User();
+        UserContext userContext = new UserContext();
+
+        public ChangePassword(string name)
+        {
+            InitializeComponent();
+            user.Name = name;
+        }
+
+        private bool IsPasswordPossible(string password)
+        {
+            if ((password.Any(c => char.IsLetter(c))) && (password.Any(c => char.IsNumber(c))))
+                return true;
+            else return false;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Help help = new Help();
+            help.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (oldPassword.Text == userContext.Users.Find(user.Name).Password.ToString())
+            {
+                if ((IsPasswordPossible(newPassword.Text) == false) && (userContext.Users.Find(user.Name).Restriction == true))
+                    MessageBox.Show("Пароль має містити букви та цифри");
+                else if (newPassword.Text == confirmPassword.Text)
+                {
+                    user.Password = newPassword.Text;
+                    userContext.Users.Find(user.Name).Password = user.Password;
+                    MessageBox.Show("Пароль успішно змінено");
+                }
+                else MessageBox.Show("Спробуйте ще раз");
+            }
+            else MessageBox.Show("Введіть вірний старий пароль");
+            userContext.SaveChanges();
+        }
+
+        private void ChangePassword_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void oldPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ OZI_lab1/Help.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace OZI_lab1
+{
+    partial class Help
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Автор: Іванченко О. В. ІС-71, варіант 6";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 62);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(207, 51);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Адміністратор:\r\nІм\'я користувача: IvanchenkoA\r\nПароль: ІванченкоZ20";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 134);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(361, 34);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Обмеження на користувацькі паролі:\r\nНаявність латинських букв, символів кирилиці " +
+    "і цифр.";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Help
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.ClientSize = new System.Drawing.Size(416, 235);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Help";
+            this.Text = "Про програму";
+            this.Load += new System.EventHandler(this.Help_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
